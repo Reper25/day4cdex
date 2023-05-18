338b4973f662f8381bacf306dab766fc8fd5ccbd
laeeqmajal <laeeq.majal@gmail.com>
2023-05-18 16:07:40 +0200

    first commit

---
+++ code.js
@@ -0,0 +1,16 @@
+let person1 = {
+    name: 'laeeq',
+    age: 25
+};
+
+let person2 = {
+    name: 'peter',
+    age: 40
+};
+
+console.log(person1);
+console.log(person2);
+
+let people = ['person1','person2'];
+
+console.log(people);
